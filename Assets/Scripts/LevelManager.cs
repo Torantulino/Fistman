b92fc500d3f9c91d14d49ae572332b008b32c19d
Torantulino <40276179@live.napier.ac.uk>
2017-10-21 02:18:17 +0100

    Basic Movement Added.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject ThePlayer;
+
+    PlayerScript Player = new PlayerScript();
+
+	// Use this for initialization
+	void Start () {
+        Player = ThePlayer.GetComponent<PlayerScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("space"))
+        {
+            //Jump!
+            Player.Jump();
+            
+        }
+
+        if (Input.GetAxis("Horizontal") != 0.0f)
+        {
+            //Move
+            float hozInput = Input.GetAxis("Horizontal");
+            Player.Move(hozInput);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //If menu isn't active
+            Debug.Log("Left click!");
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            //If menu isn't active
+            Debug.Log("Right Click!");
+        }
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    private int fistMass;
+    private int playerMass;
+    private float health;
+    private float verticalSpeed;
+    private float horizontalSpeed;
+    private Vector2 movementVector;
+    private Vector2 zero;
+    private bool isGrounded;
+    private Rigidbody2D rgdBody;
+    private LayerMask envLayerMask;
+    private float hozInput;
+    private float runSpeed;
+    private float jumpPower;
+
+
+    private void Awake()
+    {
+        rgdBody = GetComponentInChildren<Rigidbody2D>();
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        health = 5;
+        zero.x = 0;
+        zero.y = 0;
+
+        runSpeed = 4.0f;
+        jumpPower = 5.0f;
+
+        movementVector = zero;
+
+        envLayerMask = LayerMask.GetMask("Environment");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        CheckIfGrounded();
+
+        
+    }
+
+    //Use for physics
+    private void FixedUpdate()
+    {
+
+
+    }
+
+    public float Health
+    {
+        get
+        {
+            return health;
+        }
+        set
+        {
+            //Validation
+            health = value;
+        }
+    }
+
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            rgdBody.velocity = new Vector2(rgdBody.velocity.x, jumpPower);
+        }
+    }
+
+    public void Move(float direction)
+    {
+        rgdBody.velocity = new Vector2(direction * runSpeed, rgdBody.velocity.y);
+    }
+
+    public void Punch(Vector2 mouseLoc)
+    {
+        //Validation
+    }
+
+    public void HookShot(Vector2 mouseLoc)
+    {
+        //Validation (must be attached to player)
+    }
+
+    public void CheckIfGrounded()
+    {
+        RaycastHit2D hit2D = Physics2D.Raycast(rgdBody.position - new Vector2(0f, 0.5f), Vector2.down, 0.1f, envLayerMask);
+
+
+        if (hit2D)
+        {
+            isGrounded = true;
+
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+
+    }
+}
