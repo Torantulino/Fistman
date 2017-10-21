6223a4998262fd93a4648a87a4c69d433f60a307
astraTiCon <formalsystems@gmail.com>
2017-10-21 12:30:50 +0100

    added camera

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+    private Vector3 targetPos;
+    private Vector3 velocity;
+
+    // Use this for initialization
+    void Start () {
+        velocity = Vector3.zero;
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        targetPos = player.transform.position + offset;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, 0.3f);
+    }
+}
